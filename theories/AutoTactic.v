b570dc75ef5d4eacc9efcc15d2d45956b648e98f
Lasse Blaauwbroek <lasse@blaauwbroek.eu>
2022-02-09 09:06:41 +0100

    No lia

--- theories/AutoTactic.v
+++ theories/AutoTactic.v
@@ -1,4 +1,4 @@
 From Tactician Require Export Ltac1.Record.
 From Tactician Require Export Ltac1.Tactics.
-Require Export Lia.
+(* Require Export Lia. *)
 Automatic Tactics Thread (timeout 60 (try ml_synth_background)).
